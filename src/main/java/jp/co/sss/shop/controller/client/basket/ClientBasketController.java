5c5911485f7234fbb979765dd0ae0eb900a80c3f
kimura318 <kimura.tis2025@gmail.com>
2025-06-06 14:10:24 +0900

    新規作成

---
+++ src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
@@ -0,0 +1,13 @@
+package jp.co.sss.shop.controller.client.basket;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 買い物かご機能(一般会員用)のコントローラクラス
+ *
+ * @author SystemShared
+ */
+@Controller
+public class ClientBasketController {
+
+}
